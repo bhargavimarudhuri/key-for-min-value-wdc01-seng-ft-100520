049adf24dd271bfdbf580a8baef0a93f4670e3dc
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 03:28:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  keyWithMinValue = ""
-  minValue = name_hash[0]
+  firstKey = name_hash.keys[0]
+  minValue = name_hash[firstKey]
+  keyWithMinValue = firstKey
   name_hash.each do |name, val|
     if val < minValue
       minValue = val
