b83b6fe7d28384ceb152955910880afeafa5e81f
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 03:34:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,11 @@ def key_for_min_value(name_hash)
   minValue = 0
   keyWithMinValue = ""
   i = 0
+  
+  if name_hash.length == 0
+    return nil
+  end
+  
   name_hash.each do |name, val|
     if i == 0
       keyWithMinValue = name
